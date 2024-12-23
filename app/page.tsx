81830805e18501be838d92395495e2dae8301121
Hina Alvi <princess4177812@gmail.com>
2024-12-23 08:05:21 -0800

    (Solve the errors)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 
 
 import Main from "./Mainpage/page"
-import Bothpage from "./bothpage/page"
-import ServerFetching from "./Server/page"
-import Clientfetching from "./Client/page"
+
 
 export default function Home(){
 return(
